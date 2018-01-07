60231df0119a5c85066c6f6178da8436176f7f24
Ahmad Taufiq <ataufiq665@gmail.com>
2018-01-07 23:01:08 +0700

    First Commit

---
+++ App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet
+} from 'react-native';
+import { Content,Container} from 'native-base';
+import Headers from './src/components/Headers';
+import CardSwiper from './src/components/CardSwiper';
+import FooterTab from './src/components/FooterTab';
+import SearchBar from './src/components/SearchBar';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+        <Container>
+          <SearchBar />
+          <Content />
+          
+          <FooterTab />
+        </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
